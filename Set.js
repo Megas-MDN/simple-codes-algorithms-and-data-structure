35f547ee7348b2c9fc420f6820be5ed2058ab004
megas-mdn <mdn.melqui@gmail.com>
2023-10-13 10:48:53 -0300

    [Set] Implements union, intersection and diff methods

--- Set.js
+++ Set.js
@@ -1,11 +1,7 @@
-export default class Set {
+export default class Sety {
   #items;
   constructor() {
     this.#items = {};
-  }
-
-  #keyStr(key) {
-    return JSON.stringify(key);
   }
 
   has(el) {
@@ -40,10 +36,42 @@ export default class Set {
   log() {
     console.log(JSON.stringify(this.#items, null, 2));
   }
+
+  union(otherSet) {
+    const unionSet = new Set();
+    unionSet.add(...this.values());
+    unionSet.add(...otherSet.values());
+    return unionSet;
+  }
+
+  intersection(otherSet) {
+    const intersection = new Set();
+    const [setToInterate, setToCompare] =
+      otherSet.size() < this.size() ? [otherSet, this] : [this, otherSet];
+    intersection.add(
+      ...setToInterate.values().filter((item) => setToCompare.has(item))
+    );
+    return intersection;
+  }
+
+  diff(otherSet) {
+    const diffSet = new Set();
+    diffSet.add(...this.values().filter((item) => !otherSet.has(item)));
+    return diffSet;
+  }
+
+  isSubSetOf(otherSet) {
+    if (this.size() > otherSet.size()) return false;
+    const allIn = this.values().every((item) => otherSet.has(item));
+    return !!allIn;
+  }
 }
 
 const testSet = () => {
   const set = new Set();
+  set.add(1);
+  set.add(2);
+  /*
   set.add(1, 2, 3, 4, 5, 6, 7);
   console.log('Values --> ', set.values(), '<---');
   console.log('Has --> ', set.has(2), '<---');
@@ -53,6 +81,20 @@ const testSet = () => {
   console.log('Size', set.size());
   set.log();
   console.log('Values --> ', set.values(), '<---');
+  const b = new Set();
+  b.add(10, 20, 5, 1);
+  const c = set.union(b);
+  c.log();
+  const d = set.intersection(b);
+  d.log();
+  const e = b.intersection(set);
+  e.log();
+  e.add(7);
+  const f = e.diff(d);
+  f.log();
+  console.log(f.isSubSetOf(e), 'True expected');
+  console.log(f.isSubSetOf(d), 'False expected');*/
+  console.log('Array -->', [...set]);
 };
 
 testSet();
